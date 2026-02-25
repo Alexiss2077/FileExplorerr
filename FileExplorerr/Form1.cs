11e99ab43d2af4be057b0246a178b49cd8564612
Alexiss2077 <i24050023@monclova.tecnm.mx>
2026-02-25 08:18:03 -0600

    cambios

--- FileExplorerr/Form1.cs
+++ FileExplorerr/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FileExplorerr
-{
+{           /////////////////////////////7
     // ════════════════════════════════════════════════════════════════════════
     // ════════════════════════════════════════════════════════════════════════
     internal static class Theme
